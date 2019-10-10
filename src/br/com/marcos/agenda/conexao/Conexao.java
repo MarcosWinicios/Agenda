550a1973f2da8dd72b4c0849211eab135f283ed9
Marcos Winicios <pmarcoswinicios@gmail.com>
2019-10-10 15:21:04 -0300

    Adicionado metodo getConexao

--- src/br/com/marcos/agenda/conexao/Conexao.java
+++ src/br/com/marcos/agenda/conexao/Conexao.java
@@ -7,4 +7,14 @@ public class Conexao {
 	private static final String DSN = "jdb:mysql://localhost:3306/agenda?useTimezone=true&serverTimezone=UTC";
 	private static final String USUARIO = "root";
 	private static final String SENHA = "20181032";
+	
+	public Connection getConexao() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			return DriverManager.getConnection(DSN, USUARIO, SENHA);
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
