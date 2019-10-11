0ff13abcf92cb27a91599cda7c0439ad6b547ed1
Marcos Winicios <pmarcoswinicios@gmail.com>
2019-10-11 16:55:41 -0300

    Add Sevlet e Logica

---
+++ src/br/com/marcos/agenda/servlet/ControllerServlet.java
@@ -0,0 +1,32 @@
+package br.com.marcos.agenda.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.marcos.agenda.logica.Logica;
+
+
+@WebServlet("/ControllerServlet")
+public class ControllerServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String parametro = req.getParameter("logica");
+		String nomeClasse = "br.com.marcos.agenda.logica." + parametro;
+		
+		try {
+			Class<?> classe = Class.forName(nomeClasse);
+			Logica logica = (Logica) classe.getDeclaredConstructor().newInstance();
+			String pagina = logica.executa(req, resp);
+			req.getRequestDispatcher(pagina).forward(req, resp);
+			
+		}catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+ 		
+	}
+
+}
